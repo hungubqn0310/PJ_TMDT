5a58b9c354cdbeda0542414b09da026c072b6a3e
hungubqn03 <anhung03102003@gmail.com>
2024-12-04 16:39:09 +0700

    ae lam bai vui ve nha

--- 123/Helper/DatabaseHelper.cs
+++ 123/Helper/DatabaseHelper.cs
@@ -6,7 +6,7 @@ namespace _123.Helpers
 {
     public static class DatabaseHelper
     {
-        private static string _connectionString = "Server=localhost;Database=hanDK;User=root;Password=new_password;";
+        private static string _connectionString = "Server=localhost;Database=handk;User=root;Password=123456;";
 
         /// <summary>
         /// Mở kết nối đến cơ sở dữ liệu.
