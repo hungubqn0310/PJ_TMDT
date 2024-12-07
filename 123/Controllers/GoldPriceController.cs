2789ad0719e968feda3e40d7d0bfbdb200fdb1e1
DUNGDEVE <nguyenvietdung0506@gmail.com>
2024-12-07 13:39:12 +0700

    merge

--- 123/Controllers/GoldPriceController.cs
+++ 123/Controllers/GoldPriceController.cs
@@ -15,9 +15,8 @@ namespace _123.Controllers
             _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
         }
 
-        // API Key của bạn
-        private const string ApiKey = "goldapi-4af9019m49hnpo9-io";
-       
+        // API Key (nên được lấy từ file cấu hình hoặc biến môi trường)
+        private const string ApiKey = "goldapi-hqv9xsm43maetf-io";
 
         [HttpGet]
         public async Task<IActionResult> GetGoldPrice()
