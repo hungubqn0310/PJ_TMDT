cae4cb0d058786844e56ca852539f731d66c5881
VietAnh0502 <vietanhcoi2003@gmail.com>
2024-12-29 01:51:28 +0700

    update giao diện lịch sử mua hàng

--- 123/Controllers/HomeController.cs
+++ 123/Controllers/HomeController.cs
@@ -104,6 +104,11 @@ namespace _123.Controllers
         return View();
     }
 
+public IActionResult transactionHistory()
+    {
+        return View();
+    }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
